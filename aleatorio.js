f940c3b017f34fc7051a800cd26dd707beacf30d
ㅤ <vagner.oliveira24@escola.pr.gov.br>
2026-09-11 10:20:23 -0300

    Add random name selection function and export

--- aleatorio.js
+++ aleatorio.js
@@ -1,1 +1,8 @@
+const nomes = ["Fernanda", "Gabriel", "Juliana", "Vagner", "Rafael"];
 
+export function aleatorio(lista) {
+    const posicao = Math.floor(Math.random() * lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes);
